a99ce7414db0b60a4681b4dac8cea4ed4b4fadf0
Marcos Alexandria <marcos.alexandria01@gmail.com>
2025-02-14 19:43:17 -0300

    estrutura a rota de login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { DefaltLoginLayoutComponent } from "../../components/defalt-login-layout/defalt-login-layout.component";
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { InputPrimarioComponent } from '../../components/input-primario/input-primario.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [DefaltLoginLayoutComponent],
+  imports: [DefaltLoginLayoutComponent, ReactiveFormsModule, InputPrimarioComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm!: FormGroup;
 
+  constructor(){
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    })
+  }
 }

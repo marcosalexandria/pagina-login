9425d383aee2d22fb582f187c3ffd261118bec0a
Marcos Alexandria <marcos.alexandria01@gmail.com>
2025-02-14 18:31:35 -0300

    estruturando o login-layout

--- src/app/components/defalt-login-layout/defalt-login-layout.component.ts
+++ src/app/components/defalt-login-layout/defalt-login-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-defalt-login-layout',
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './defalt-login-layout.component.scss'
 })
 export class DefaltLoginLayoutComponent {
-
+  @Input() titulo: string = "";
+  @Input() textoBotaoPrimario: string = "";
+  @Input() textoBotaoSegundario: string = "";
 }

a90f0394465335cdee5fa422c367dab895721542
Marcos Alexandria <marcos.alexandria01@gmail.com>
2025-02-14 21:26:45 -0300

    cria a tela de cadastro

--- src/app/pages/cadastro/cadastro.component.ts
+++ src/app/pages/cadastro/cadastro.component.ts
@@ -6,34 +6,46 @@ import { Route, Router } from '@angular/router';
 import { LoginService } from '../../services/login.service';
 import { ToastrService } from 'ngx-toastr';
 
+interface cadastroForm{
+  nome: FormControl,
+  email: FormControl,
+  password: FormControl,
+  confirmaPassword: FormControl
+
+}
+
 @Component({
-  selector: 'app-login',
+  selector: 'app-cadastro',
   standalone: true,
   imports: [DefaltLoginLayoutComponent, ReactiveFormsModule, InputPrimarioComponent],
   providers: [
     LoginService
   ],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  templateUrl: './cadastro.component.html',
+  styleUrl: './cadastro.component.scss'
 })
-export class LoginComponent {
-  loginForm!: FormGroup;
+export class CadastroComponent {
+  cadastroForm!: FormGroup<cadastroForm>;
 
   constructor(private router: Router, private loginService: LoginService, private toastService: ToastrService){
-    this.loginForm = new FormGroup({
+
+    this.cadastroForm = new FormGroup({
+      nome: new FormControl('', [Validators.required, Validators.minLength(3)]),
       email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      confirmaPassword: new FormControl('', [Validators.required, Validators.minLength(6)])
     })
+
   }
 
   entrar(){
-    this.loginService.entrar(this.loginForm.value.email, this.loginForm.value.password).subscribe({
+    this.loginService.entrar(this.cadastroForm.value.email, this.cadastroForm.value.password).subscribe({
       next: () => this.toastService.success("login feito com sucesso!"),
       error: () => this.toastService.error("Erro, tente novamente mais tarde!")
     })
   }
 
   navegaParaCadastro(){
-    this.router.navigate(["cadastro"]) //inda nao criei a rota
+    this.router.navigate(["login"]) //inda nao criei a rota
   }
 }
